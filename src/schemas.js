b3af0133d473e763fb9847f94ba33827b06d7ee8
Bobby Grayson <53058768+notactuallytreyanastasio@users.noreply.github.com>
2025-06-20 00:58:04 -0400

    delete the typescript app, it served its purpose

--- src/schemas.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.VIBE_MEMBER_NSID = exports.VIBE_NSID = exports.COMMENT_NSID = exports.GROOVE_NSID = exports.BLIP_NSID = void 0;
-exports.BLIP_NSID = 'com.blonk.blip';
-exports.GROOVE_NSID = 'com.blonk.groove';
-exports.COMMENT_NSID = 'com.blonk.comment';
-exports.VIBE_NSID = 'com.blonk.vibe';
-exports.VIBE_MEMBER_NSID = 'com.blonk.vibeMember';
